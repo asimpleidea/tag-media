f7d38a6941a11afcce0f4ea8c1fa1f6e87903bc5
Elis Lulja <elulja@cisco.com>
2023-04-14 20:28:33 +0200

    feat(media): add `MediaFile` data.
    Signed-off-by: Elis Lulja <elulja@cisco.com>

---
+++ src/data/media_file.rs
@@ -0,0 +1,66 @@
+use diesel::{
+    backend::Backend,
+    deserialize::{self, FromSql},
+    sql_types::Text,
+    Queryable,
+};
+use serde::Serialize;
+
+/// This represents a media type.
+#[derive(Debug, Serialize)]
+pub enum MediaType {
+    Unknown,
+    Image,
+    Video,
+    Sound,
+}
+
+impl Into<MediaType> for String {
+    fn into(self) -> MediaType {
+        match self.as_str() {
+            "image" => MediaType::Image,
+            "video" => MediaType::Video,
+            "sound" => MediaType::Sound,
+            _ => MediaType::Unknown,
+        }
+    }
+}
+
+// Needed for a good deserialization for [`MediaType`].
+impl<DB> Queryable<Text, DB> for MediaType
+where
+    DB: Backend,
+    String: FromSql<Text, DB>,
+{
+    type Row = String;
+
+    fn build(s: String) -> deserialize::Result<Self> {
+        Ok(s.into())
+    }
+}
+
+/// This represents a media file.
+#[derive(Debug, Queryable, Serialize)]
+#[diesel(table_name = media)]
+pub struct MediaFile {
+    /// The ID of the file.
+    pub id: i64,
+    /// The path relative to its base path.
+    pub relative_path: String,
+    /// The ID of the path that this belongs to.
+    pub base_path_id: i32,
+    /// The width, if an image or video.
+    pub width: Option<i16>,
+    /// The height, if an image or video.
+    pub height: Option<i16>,
+    /// The size of the file in kB.
+    pub size: f64,
+    /// The mark, from 1 to 10.
+    pub mark: Option<i16>,
+    /// The description for this file.
+    pub description: String,
+    /// The type of this file, e.g. `Image`, `Video` or `Sound`.
+    /// See [`MediaType`]
+    #[diesel(serialize_as = String)]
+    pub media_type: MediaType,
+}
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,3 +1,4 @@
 pub mod base_path;
+pub mod media_file;
 pub mod tag;
 pub mod tag_category;
